726d0ad01156f88a974883724cd98abe17823533
kimdaeyeub <kdy3915@naver.com>
2023-12-01 14:04:08 +0900

    AvatarUrl 수정

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,12 +2,16 @@ import User from '../models/User';
 
 export const postJoin = async (req, res) => {
   const { username, password, email, avatarUrl } = req.body;
-
+  let image = '';
   //유저의 입력정보가 전달이 잘 되지 않을때의 에러
   if (username === '' || password === '' || email === '') {
     return res.send({
       message: '유저네임, 이메일, 비밀번호는 필수입력 요소입니다.',
     });
+  }
+
+  if (avatarUrl) {
+    image = avatarUrl;
   }
 
   //해당 정보의 유저가 이미 존재할때의 에러핸들링
@@ -16,7 +20,12 @@ export const postJoin = async (req, res) => {
     return res.send({ message: '해당 정보의 유저가 이미 존재합니다.' });
   }
 
-  const newUser = await User.create({ username, password, email, avatarUrl });
+  const newUser = await User.create({
+    username,
+    password,
+    email,
+    avatarUrl: image,
+  });
   return res.json(newUser);
 };
 
@@ -42,7 +51,7 @@ export const postLogin = async (req, res) => {
 export const getMe = async (req, res) => {
   try {
     const { user, loggedIn } = req.session;
-    const { email, password, username } = user;
+    const { email, password, username, avatarUrl } = user;
 
     console.log(user);
 
@@ -50,7 +59,7 @@ export const getMe = async (req, res) => {
       return res.status(401).json({ error: 'User not authenticated' });
     }
 
-    return res.json({ email, password, username });
+    return res.json({ email, password, username, avatarUrl });
   } catch (error) {
     console.error('Error in getMe:', error);
     return res.status(500).json({ error: 'Internal Server Error' });
--- src/models/User.js
+++ src/models/User.js
@@ -16,6 +16,7 @@ const userSchema = new mongoose.Schema({
   },
   avatarUrl: {
     type: String,
+    required: [true, 'AvatarUrl is required'],
   },
 });
 
